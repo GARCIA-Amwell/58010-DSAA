913f6a56638c074646fba119a56c0f882bd78a0d
GARCIA-Amwell <123443470+GARCIA-Amwell@users.noreply.github.com>
2023-09-07 15:52:57 +0800

    Add files via upload

---
+++ Sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	    cout<<"Original Array: 5, 4, 3, 2, 1 " <<endl;
+	    cout <<"New Array: ";
+    int n = 5;
+    int arr[n] = {5, 4, 3, 2, 1};
+    int temp;
+    for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+        
+    }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    
+}
